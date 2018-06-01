19c181bf8536f6d549691d3d39c147ce2c93e541
jonghyun <heiwais25@gmail.com>
2018-06-02 03:45:25 +0900

    Fix the problem from np.arctan by changing it to np.arctan2

--- cameraAnalysis/utility.py
+++ cameraAnalysis/utility.py
@@ -1,4 +1,8 @@
+import itertools
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LogNorm
+
 
 def sphe_to_cart(sphe_coord):
     """Change spherical coordinates to spherical coordinate
@@ -28,7 +32,8 @@ def cart_to_sphe(cart_coord):
         x, y, z = cart_coord
     else:
         x, y, z = cart_coord[:, 0], cart_coord[:, 1], cart_coord[:, 2]
-    return np.array([np.arctan(y/x), np.arccos(z)]).T
+    return np.array([np.arctan2(y, x), np.arccos(z)]).T
+    
 
 def calculate_inter_angle_to_camera(camera_normal_vec, photon_dir_vec):
     """It wil calculate the angle between camera normal vector and photon track directional vector
@@ -49,7 +54,7 @@ def calculate_inter_angle_to_camera(camera_normal_vec, photon_dir_vec):
 
 
 def get_sensor_filter(photon_pos_sphe, photon_momentum_sphe, camera_pos_sphe, 
-                      radius=0.16510, camera_distance=0.02, camera_lens_rad = 0.005, opening_angle = np.pi * 2 / 3):
+                      radius=0.16510, camera_distance=0.02, camera_lens_rad = 0.005, FOV = 120):
     """From the boundary, check photon can arrive in lens or not. There are main two parts
     1. Check lens geometry and photon track
     2. Compare opening angle
@@ -61,7 +66,7 @@ def get_sensor_filter(photon_pos_sphe, photon_momentum_sphe, camera_pos_sphe,
         radius (float) : radius of boundary which photon pos are located
         camera_distance (float) : how much far from boundary
         camera_lens_rad (float) : lens radius of camera
-        opening_angle (float) : opening angle of camera
+        FOV (float) : opening angle of camera [deg]
 
     Returns:
         np.array([bool]) : condition that photon will arrive in lens or not
@@ -97,7 +102,7 @@ def get_sensor_filter(photon_pos_sphe, photon_momentum_sphe, camera_pos_sphe,
     inter_angle = calculate_inter_angle_to_camera(camera_normal_vec, arrival_dir_vec)
     
     # Combine all condtion to arrive in sensor
-    sensor_filter = is_arrive_sensor * (inter_angle < opening_angle / 2)
+    sensor_filter = is_arrive_sensor * (inter_angle < np.deg2rad(FOV) / 2) * (inter_angle > 0)
     
     return sensor_filter
 
@@ -134,7 +139,7 @@ def calc_refracted_dir(s1, N, n1, n2):
     cross = np.cross(N,s1)
     # print(cross)
     # print(n**2 * np.sum(cross * cross, axis=1))
-    s2 = n * np.cross(N, -cross) - (N.T * np.sqrt(1 - n**2 * np.sum(cross * cross, axis=1))).T
+    s2 = n * np.cross(N, np.cross(-N, s1)) - (N.T * np.sqrt(1 - n**2 * np.sum(cross * cross, axis=1))).T
     return s2
 
 
@@ -155,10 +160,12 @@ def pass_pressure_vessel(photon_pos_sphe, photon_momentum_sphe, n0, r0 = 0.16510
     r1 = r0 - d0
     
     # step 1 (ice -> vessel)
-    s1 = sphe_to_cart(photon_momentum_sphe) * -1 # inicident angle
+    s1 = -1 * sphe_to_cart(photon_momentum_sphe)  # inicident angle
     N = sphe_to_cart(photon_pos_sphe)
     s2 = calc_refracted_dir(s1, N, n0, n1)
     
+    # print(s1, s2)
+
     # Move from outer boundary to inner boundary inside of vessel 
     # It is just solving simultaneous equations of sphere(inner vessel) and refracted photon track
     p0 = r0 * sphe_to_cart(photon_pos_sphe)
@@ -191,5 +198,234 @@ def pass_pressure_vessel(photon_pos_sphe, photon_momentum_sphe, n0, r0 = 0.16510
     # With nan filter, calculate momentum and position of photon after passing the vessel
     new_momentum_sphe = cart_to_sphe(-s2[nan_filter].reshape(-1, 3))
     new_pos_sphe = cart_to_sphe(N[nan_filter].reshape(-1, 3))
-    
+
     return new_momentum_sphe, new_pos_sphe
+
+
+def get_total_hist2d(file_name, num_files, 
+                     x_edges = np.linspace(-np.pi, np.pi, 100), 
+                     y_edges = np.linspace(0, np.pi, 100),
+                     bins = [],
+                     r0 = 0.16510, #[m]
+                     d0 = 0.0127, #[m]
+                     wv = 405, 
+                     camera_pos_sphe = np.array([0, np.pi/2]), 
+                     camera_lens_rad = 0.005,
+                     filter_lens = True,
+                     show_position = False, 
+                     src_dir = "../dats/"):
+    """Because the large photon data like more than 10^12 is stored in separately. So, we need to merge splitted data.
+    It will be used for the purpose of comparing histogram.
+    
+    Args:
+        file_name(str) : name of the first file
+        num_files(int) : how much file exist or  will be read
+        x_edges (np.array([phi])) : bins of phi for histogram
+        y_edges (np.array([theta])) : bins of theta for histogram
+        bins(int) : number of bins for the histogram
+        r0(float) : DOM radius
+        d0(float) : Vessel thickness
+        wv(float) : wavelength of the photon from the LED [nm]
+        camera_pos_sphe(np.array([phi, theta])) : camera position vector from the center of DOM
+        camera_lens_rad(float) : radius of camera lens
+        filter_lens(bool) : If it is true, calculate histogram for photons which are on the lens. Otherwise just collect all momentum info
+        
+
+    Returns:
+        H : Total histogram of photon on the sensor
+        x_edges : x_edges used for the histogram
+        y_edges : y_edges used for the histogram
+    """
+    # Get photon log from input file name
+    H = []
+    momentum = []
+    direction = []
+    for i in range(num_files):
+        # Read photon log
+        this_file_name = file_name.replace('0.dat', '%d.dat' % i)
+        data = np.fromfile(src_dir + this_file_name, dtype='<f4')
+        photon_log = data.reshape(-1, 8)
+        momentum= photon_log[:,5:3:-1]
+        direction = photon_log[:,7:5:-1]
+        radius = r0
+        if filter_lens:
+            # Apply refraction on the pressure vessel
+            if d0 != 0:
+                momentum, direction = pass_pressure_vessel(direction, momentum, refractive_index_ice(wv))
+                radius -= d0
+
+            # Get sensor area filter
+            refracted_filter = get_sensor_filter(direction, momentum, camera_pos_sphe, 
+                                                    camera_lens_rad=camera_lens_rad, 
+                                                    radius=radius)
+            
+            # Apply the filter
+            momentum = momentum[refracted_filter]
+            direction = direction[refracted_filter]
+
+        print('%dth file : %d photon arrive in lens' %(i+1, len(momentum)))
+
+        if not show_position:
+            data = momentum
+        else:
+            data = direction
+
+
+        # Put the data into histogram
+        if isinstance(bins, int):
+            H0, y_edges, x_edges = np.histogram2d(data[:,1], data[:,0], bins=bins)
+        elif len(bins) == 2:
+            H0, y_edges, x_edges = np.histogram2d(data[:,1], data[:,0], bins=[bins[1], bins[0]])
+        elif len(bins) == 0:
+            H0, y_edges, x_edges = np.histogram2d(data[:,1], data[:,0], bins=(y_edges, x_edges))
+        else:
+            raise Exception('Invalid bins error')
+        
+        if i == 0:
+            H = H0
+        else:
+            H += H0
+
+    print('The number of total photon on the lens : %d' %(np.sum(H)))
+    return H, x_edges, y_edges
+
+
+def draw_arrival_info(momentum, position, mode, ice_param, diff,
+                          figsize=(12,6), 
+                          bins=50, 
+                          fontsize=14, 
+                          output_dir = "", 
+                          refraction=False,
+                          savefig=False, 
+                          showfig=False):
+    """Draw raw arrival info like momentum and direction
+    
+    Args : 
+        momentum (np.array([phi, theta])) : arrival photon track momentum
+        position (np.array([phi, theta])) : arrival photon position
+        mode : which data you will use for plotting
+        ice_param (float, float) : scattering length and absorption length
+        diff(float, float, float) : difference of x, y, z between two DOM
+        figsize (int, int) : figure size
+        refraction (bool) : whether we use data after refraction 
+    """
+    if mode == "position":
+        data = position
+    elif mode == "momentum":
+        data = momentum
+    else:
+        raise Exception('invalid mode : %s' % mode)
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.rcParams['font.size']=fontsize
+    h = plt.hist2d(data[:,0], data[:,1], bins=bins, norm=LogNorm());
+    plt.colorbar(h[3])
+    plt.xlabel("Arrival %s [rad]" % (r'$\varphi$'))
+    plt.ylabel("Arrival %s [rad]" % (r'$\theta$'))
+    title = 'Arrival %s of the photon\n[$l_{scat}$ = %.2fm, $l_{abs}$ = %.2fm, %dm interval]' \
+          % (mode, ice_param[0], ice_param[1], diff[0])
+    file_name = 'arrival_%s_scat_%.2f_abs_%.2f_%d.png' % (mode, ice_param[0], ice_param[1], diff[0])
+    if refraction:
+        title = title.replace('photon', 'photon after refraction')
+        file_name = file_name.replace('arrival', 'arrival_refraction')
+    plt.title(title)
+        
+    
+    if savefig:
+        plt.savefig(output_dir + file_name, dpi=400)
+    if not showfig:
+        plt.close()
+
+def draw_FOV_boundary(camera_pos_sphe, FOV, output_dir="", fontsize=14, savefig=False, showfig=False):
+    """Draw FOV boundary for two axis(phi, theta). The boundary is used for filtering photon track in next step
+    
+    Args:
+        camera_pos_sphe (np.array(phi, theta)) : camera position as spherical coordinate
+        FOV (int) : FOV of camera [deg]
+    """
+    
+    theta = np.linspace(0, np.pi, 100)
+    phi = np.linspace(-np.pi, np.pi, 100)
+    test_cases = np.array(list(itertools.product(phi, theta)))
+    test_angle = calculate_inter_angle_to_camera(sphe_to_cart(camera_pos_sphe), -sphe_to_cart(test_cases))
+    
+    half_opening_angle = np.deg2rad(FOV) / 2
+    fig, ax = plt.subplots(figsize=(12, 6))
+    plt.rcParams['font.size']=fontsize
+
+    CS = plt.contour(test_angle.reshape(100, 100).T, levels = [half_opening_angle], extent=[-np.pi, np.pi, 0, np.pi])
+    plt.clabel(CS, inline=1, fontsize=10)
+    plt.xlabel("Arrival %s [rad]" % (r'$\varphi$'))
+    plt.ylabel("Arrival %s [rad]" % (r'$\theta$'))
+    plt.title('FOV boundary (%d$\degree)$' % FOV)
+    if savefig:
+        plt.savefig(output_dir + 'FOV_boundary_%d.png' % FOV, dpi=400)
+    if not showfig:
+        plt.close()
+
+def draw_photon_with_lens(momentum, position, camera_pos_sphe, FOV, mode, ice_param, diff,
+                          figsize=(12,6), 
+                          bins=50, 
+                          fontsize=14, 
+                          xlim=[], 
+                          ylim=[],
+                          output_dir = "",
+                          showall=False,
+                          drawFOV=False,
+                          onlylens=False,
+                          refraction=False,
+                          savefig=False, 
+                          showfig=False):
+    """Draw photon on the lens after refraction
+    
+    """
+    if mode == "position":
+        data = position
+    elif mode == "momentum":
+        data = momentum
+    else:
+        raise Exception('invalid mode : %s' % mode)
+    
+    # For drawing FOV
+    theta = np.linspace(0, np.pi, 100)
+    phi = np.linspace(-np.pi, np.pi, 100)
+    test_cases = np.array(list(itertools.product(phi, theta)))
+    test_angle = calculate_inter_angle_to_camera(sphe_to_cart(camera_pos_sphe), -sphe_to_cart(test_cases))
+    half_opening_angle = np.deg2rad(FOV) / 2
+    
+    fig, ax = plt.subplots(figsize=figsize)
+    plt.rcParams['font.size']=fontsize
+
+    if drawFOV:
+        CS = plt.contour(test_angle.reshape(100, 100).T, levels = [half_opening_angle], extent=[-np.pi, np.pi, 0, np.pi])
+        CS.collections[0].set_label("FOV/2")
+        plt.clabel(CS, inline=1, fontsize=10)
+        plt.legend()
+        
+    h = plt.hist2d(data[:,0], data[:,1], bins=bins, norm=LogNorm());
+    plt.colorbar(h[3])
+    
+    if len(xlim) != 0:
+        plt.xlim(xlim)
+    if len(ylim) != 0:
+        plt.ylim(ylim)
+    if showall:
+        plt.xlim([-np.pi, np.pi])
+        plt.ylim([0, np.pi])
+        
+    
+    plt.xlabel("Arrival %s [rad]" % (r'$\varphi$'))
+    plt.ylabel("Arrival %s [rad]" % (r'$\theta$'))
+    
+    title = 'Arrival %s of the photon with FOV\n[$l_{scat}$ = %.2fm, $l_{abs}$ = %.2fm, %dm interval]' \
+          % (mode, ice_param[0], ice_param[1], diff[0])
+    file_name = 'arrival_%s_with_%d_FOV_scat_%.2f_abs_%.2f_%d.png' % (mode, FOV, ice_param[0], ice_param[1], diff[0])
+    
+    if onlylens:
+        title = title.replace('FOV', 'FOV(on lens area)')
+        file_name = file_name.replace('FOV', 'FOV_lens_area')
+        
+    plt.title(title)
+    if savefig:
+        plt.savefig(output_dir + file_name, dpi=400)
+    if not showfig:
+        plt.close()
